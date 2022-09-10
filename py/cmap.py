2b6b12cf206304f99674d7b57f515a8c5df6e377
biprateep <biprateep@gmail.com>
2022-09-10 16:05:44 -0400

    Add function to get white for zero values

--- py/cmap.py
+++ py/cmap.py
@@ -1,7 +1,13 @@
 import numpy as np
 from matplotlib import cm
-from matplotlib import colors
 from matplotlib.colors import ListedColormap
 
-def cmap_white():
-  return 0
+
+def cmap_white(cmap_name):
+    """Returns a colormap with white as the lowest value color."""
+    cmap = cm.get_cmap(cmap_name, 256)
+    newcolors = cmap(np.linspace(0, 1, 256))
+    white = np.array([1, 1, 1, 1])
+    newcolors[:1, :] = white
+    cmap_white = ListedColormap(newcolors)
+    return cmap_white
