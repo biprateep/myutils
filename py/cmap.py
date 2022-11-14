c6d22a3cf1118de65b5d618a81d909d8e17cd666
Biprateep Dey <biprateep@users.noreply.github.com>
2022-11-14 12:31:21 -0500

    add support for seaborn names

--- py/cmap.py
+++ py/cmap.py
@@ -1,13 +1,15 @@
-import numpy as np
-from matplotlib import cm
-from matplotlib.colors import ListedColormap
-
-
 def cmap_white(cmap_name):
     """Returns a colormap with white as the lowest value color."""
-    cmap = cm.get_cmap(cmap_name, 256)
+    import numpy as np
+    try:
+        from matplotlib import cm
+        from matplotlib.colors import ListedColormap
+        cmap = cm.get_cmap(cmap_name, 256)
+    except ValueError:
+        import seaborn as sns
+        cmap = sns.color_palette("flare", as_cmap=True)
     newcolors = cmap(np.linspace(0, 1, 256))
-    white = np.array([1, 1, 1, 1])
+    white = np.array([1, 1, 1, 0])
     newcolors[:1, :] = white
     cmap_white = ListedColormap(newcolors)
     return cmap_white
